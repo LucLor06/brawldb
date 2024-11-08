095abd67675bd95c32259b97ac466306e8ac4d74
LucLor06 <LukeLorenzBA@gmail.com>
2024-11-08 02:20:29 +0000

    Change environment variable name for secret key.

--- brawldb/environment_variables.py
+++ brawldb/environment_variables.py
@@ -5,4 +5,4 @@ current_dir = os.path.dirname(os.path.abspath(__file__))
 path = os.path.join(current_dir, '.env')
 load_dotenv(path)
 
-SECRET_KEY = os.getenv("SECRET_KEY")
+DJANGO_SECRET_KEY = os.getenv("BRAWLDATABASE_SECRET_KEY")
